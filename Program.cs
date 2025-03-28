2ec397b0fd68a3ea63bbc297cc5a007ae2aaba9f
eugene mark <eugenemark@live.com>
2025-03-28 16:30:51 +0800

    Add Roles Access Logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using EugeneCV.Data;
 using EugeneCV.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +8,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login";
+        options.AccessDeniedPath = "/Unauthorized";
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOrUser", policy => policy.RequireRole("Admin", "User"));
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+});
+
 builder.Services.AddScoped<IUserService, UserService>(); //inject user service DI
+builder.Services.AddScoped<ICVManagementService, CVManagementService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSession(); 
+builder.Services.AddSession();
 
 
 var app = builder.Build();
@@ -29,7 +45,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
