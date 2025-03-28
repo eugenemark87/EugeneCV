0b9cbe030329b643594097c1f10d95f6915c19fa
eugene mark <eugenemark@live.com>
2025-03-28 16:28:39 +0800

    inject management service DI, add roles access policy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,40 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EugeneCV.Models;
+using EugeneCV.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 
 namespace EugeneCV.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ICVManagementService _cvManagementService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ICVManagementService cVManagementService)
     {
         _logger = logger;
+        _cvManagementService = cVManagementService;
     }
 
-    public IActionResult Index()
+    [Authorize(Policy = "AdminOrUser")]
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var cv = await _cvManagementService.GetCVAsync();
+        return View(cv);
     }
-
+    [Authorize(Policy = "AdminOrUser")]
     public IActionResult Privacy()
     {
         return View();
     }
 
-    public IActionResult Logout()
+    public async Task<IActionResult> Logout()
     {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         HttpContext.Session.Clear();
         return RedirectToAction("Index", "Login");
     }
