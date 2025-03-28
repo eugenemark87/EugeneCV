41d6d10bb70436b9e0c0aba35eac76c52da0791f
eugene mark <eugenemark@live.com>
2025-03-28 16:19:57 +0800

    Update user service

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -5,5 +5,8 @@ namespace EugeneCV.Services
     public interface IUserService
     {
         Task<User?> AuthenticateUserAsync(string username, string password);
+        Task<string?> GetUserRole(int roleId);
+        Task SaveLoggedInDate(int userId);
+
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,6 +2,7 @@
 using EugeneCV.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Drawing;
+using System.IO.Enumeration;
 
 namespace EugeneCV.Services
 {
@@ -20,9 +21,36 @@ namespace EugeneCV.Services
 
             if (obj == null || !BCrypt.Net.BCrypt.Verify(password, obj.Password)) //verify against hashed password
             {
-                return null; 
+                return null;
             }
-                return obj; 
+            return obj;
         }
+
+        public async Task<string> GetUserRole(int roleId)
+        {
+            var obj = await _dbContext.Roles.FirstOrDefaultAsync(u => u.RoleId == roleId);
+
+            if (obj != null)
+            {
+                return obj.Name;
+            }
+            return "";
+        }
+
+        public async Task SaveLoggedInDate(int userId)
+        {
+            var obj = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            if (obj != null)
+            {
+                obj.LastLoggedIn = System.DateTime.Now;
+
+                _dbContext.Users.Update(obj);
+                await _dbContext.SaveChangesAsync();
+
+            }
+         
+        }
+
+
     }
 }
